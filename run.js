badbd2aac654622426c6ecaa868864e75a6a59c8
Tri <bayu165kusuma@gmail.com>
2020-07-06 22:06:43 +0700

    Final Run Count Machine OOP Import

---
+++ run.js
@@ -0,0 +1,19 @@
+import MesinHitung from './mesinhitung.js'
+
+const Pi = 3.14;
+
+let mh = new MesinHitung();
+mh.add(10).subtract(5).result();
+mh.add(3).multiply(4).divide(6).result();
+
+mh.x = 7;
+console.log(`nilai sekarang : ${mh.x}`);
+mh.multiply(2).multiply(Pi).result();
+
+mh.x = 7;
+mh.square().multiply(Pi).result();
+
+mh.x = 4;
+mh.exponent(3).result();
+mh.squareRoot().result();
+
